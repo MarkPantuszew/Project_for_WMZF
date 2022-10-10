e4ef8e016228d462c4c21a58929debcf77c1df98
studentwftims <studentwftims@lab.mif>
2022-10-10 09:27:46 +0200

    ALL

---
+++ main.py
@@ -0,0 +1,18 @@
+imie_2 = 'Magda'
+moje_2imie = 'Lena'
+drugie_imie = 'Mark'
+print('imie_2')
+print('moje_2imie')
+print('drugie_imie')
+
+print(2>3)
+print('b' < 'c')
+print('-------------------------------------------------------------------------------------')
+print(bool(33))
+print(bool('False'))
+print(bool(''))
+print('-------------------------------------------------------------------------------------')
+print(10//3)
+print(10//-3)
+print(10 ** 3)
+y = 3; y +=1; print(y)
