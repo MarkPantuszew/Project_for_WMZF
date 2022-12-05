a3e96b1a7f1c6d6fde8c0cfef3ff188087e93fb0
MarkPantushew <s191632@student.pg.edu.pl>
2022-12-05 09:35:58 +0100

    Twoja proba

--- Project_for_WMZF/WMZF_project.py
+++ Project_for_WMZF/WMZF_project.py
@@ -1,12 +1,11 @@
-import numpy as np
-def drgania_mech(a, w, f, m, p):
-    a = float(input('Podaj wartosc amplitudy dla drgan mechanicznych: '))  # amplituda
-    w = float(input('Podaj wartosc czestosci wlasnej dla drgan mechanicznych: '))  # czestosc wlasna
-    f = float(input('Podaj wartosc wspolczynnika oporu ruchu dla drgan mechanicznych: '))  # wsp_op_ruchu
-    m = float(input('Podaj wartosc masy dla drgan mechanicznych: '))  # masa
-    p = float(input('Podaj wartosc amplitudy dla drgan mechanicznych: '))  # faza_poczatkowa
-    t = np.linespace(0, 1000, 10000000)
-    x = a * (np.exp(-(f/2m) * t)) * np.sin((np.sqrt(w ** 2 - (f/2m) ** 2)) * t + p))
-    plt.plot(t, x, color='r', label='dr.mech')
-    plt.show()
-    return drgania_mech(a,w,f,m,p)
+# ###
+# import numpy as np
+# a=input('Podaj wartosc amplitudy dla drgan mechanicznych: ')#amplituda
+# w=input('Podaj wartosc czestosci wlasnej dla drgan mechanicznych: ')#czestosc wlasna
+# f=input('Podaj wartosc wspolczynnika oporu ruchu dla drgan mechanicznych: ')#wsp_op_ruchu
+# m=input('Podaj wartosc masy dla drgan mechanicznych: ')#masa
+# p=input('Podaj wartosc amplitudy dla drgan mechanicznych: ')#faza_poczatkowa
+# def drgana_mech(amplituda, czestosc,wsp_op_ruchu,masa, faza_pocz):
+#     return drgana_mech(a,w,f,m,p)
+# t = np.linespace(0,1000,10000000 )
+# x=a * (np.exp(-(f/2m)*t)) * np.sin((np.sqrt(w**2-(f/2m)**2))*t + p)
