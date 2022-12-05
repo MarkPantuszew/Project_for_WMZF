e4681624470659f34ccbaa5c39956bdc3ab44e8a
Kamil Nowak <s189406@o365.student.pg.edu.pl>
2022-12-05 09:33:14 +0100

    Kamil - projekt1

--- Project_for_WMZF/WMZF_project.py
+++ Project_for_WMZF/WMZF_project.py
@@ -1,8 +1,12 @@
-#gdzie kucharek szesc tam nie ma co jesc
-def fun(n):
-    return n**2
-print(fun(87))
-#kurwa nie dziala
-#chyba dziala
-#Kamil, napisz cos i zcommituj jo jo
-#Kamil - próba :ooooooooooooo
+import numpy as np
+def drgania_mech(a, w, f, m, p):
+    a = float(input('Podaj wartosc amplitudy dla drgan mechanicznych: '))  # amplituda
+    w = float(input('Podaj wartosc czestosci wlasnej dla drgan mechanicznych: '))  # czestosc wlasna
+    f = float(input('Podaj wartosc wspolczynnika oporu ruchu dla drgan mechanicznych: '))  # wsp_op_ruchu
+    m = float(input('Podaj wartosc masy dla drgan mechanicznych: '))  # masa
+    p = float(input('Podaj wartosc amplitudy dla drgan mechanicznych: '))  # faza_poczatkowa
+    t = np.linespace(0, 1000, 10000000)
+    x = a * (np.exp(-(f/2m) * t)) * np.sin((np.sqrt(w ** 2 - (f/2m) ** 2)) * t + p))
+    plt.plot(t, x, color='r', label='dr.mech')
+    plt.show()
+    return drgania_mech(a,w,f,m,p)
