9ca9fd5c7e2cb268a69491b97844519e16303d70
MarkPantuszew <119132215+MarkPantuszew@users.noreply.github.com>
2022-12-08 21:24:43 +0100

    Update WMZF_project.py

--- Project_for_WMZF/WMZF_project.py
+++ Project_for_WMZF/WMZF_project.py
@@ -1,3 +1,5 @@
+import numpy as np
+import matplotlib as plt
 a = 0
 while a ==0:
     opcja_1 =input('Wpisz liczbe "1" zeby przejsc do drgan mechanicznych albo liczbe "2" zeby wybrac grdania w ukladzie RLC: ')
@@ -10,33 +12,85 @@ while a ==0:
     except:
         print("Podajes nie liczbe!")
     if opcja_1==1:
-            print(4)
-        #tu wstawimy drgania mechaniczne
-    if opcja_1==2:                                                                 #ewentualnie z else tez mozna przejscie wymysliec
-        #a tu RLC
-            print(455677777)
-            
-# import numpy as np
-#
-#
-# # import matplotlib as plt
-#
-# def drgania_mech():
-#     a = float(input('Podaj wartosc amplitudy dla drgan mechanicznych: '))  # amplituda
-#     w = float(input('Podaj wartosc czestosci wlasnej dla drgan mechanicznych: '))  # czestosc wlasna
-#     f = float(input('Podaj wartosc wspolczynnika oporu ruchu dla drgan mechanicznych: '))  # wsp_op_ruchu
-#     m = float(input('Podaj wartosc masy dla drgan mechanicznych: '))  # masa
-#     p = float(input('Podaj wartosc fazy poczatkowej dla drgan mechanicznych: '))  # faza_poczatkowa
-#     t = 1
-#     x = a * (np.exp(-f / (2 * m)) * t) * np.sin((np.sqrt((w ** 2) - ((f / (2 * m)) ** 2)) * t) + p)
-#     return x
-# a, w, f, m, p, t, x = drgania_mech()
-# print(x)
+        # def drgania_mech():
+        #     a = float(input('Podaj wartosc amplitudy dla drgan mechanicznych: '))  # amplituda
+        #     w = float(input('Podaj wartosc czestosci wlasnej dla drgan mechanicznych: '))  # czestosc wlasna
+        #     f = float(input('Podaj wartosc wspolczynnika oporu ruchu dla drgan mechanicznych: '))  # wsp_op_ruchu
+        #     m = float(input('Podaj wartosc masy dla drgan mechanicznych: '))  # masa
+        #     p = float(input('Podaj wartosc fazy poczatkowej dla drgan mechanicznych: '))  # faza_poczatkowa
+        #     t = 1
+        #     x = a * (np.exp(-f / (2 * m)) * t) * np.sin((np.sqrt((w ** 2) - ((f / (2 * m)) ** 2)) * t) + p)
+        #     return x
+        # x = drgania_mech()#trzeba uzuyc funkcji global
+        # print(x)
+        print('ty kistiu' )
+        b = 0
+        while b == 0:
+            wybor = int(input('Wpisz liczbe "1" zeby wprowadzic wartosc czasu albo liczbe "2" zeby wartosc czasu ustanowila sie domyslnie: '))
+            match wybor:
+                case 1:
+                    czas=float(input('Podaj wartosc czasu, przeciez tego chciales kurwa: '))#tu nie trzeba obslugiwac wyjatki
+                    b=1
+                case 2:
+                    t=1
+                    print(t)
+                    b=1
+                case _:
+                    print("Nie mozna tego uzyc!Wybierz 1 albo 2")
 
 
 
 
 
+
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
 import numpy as np
 def drgania_mech(a, w, f, m, p):
     a = float(input('Podaj wartosc amplitudy dla drgan mechanicznych: '))  # amplituda
