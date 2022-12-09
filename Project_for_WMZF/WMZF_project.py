44837d01099134d4307090f369411aa0bee917c5
Kamil Nowak <119288493+K4YM1L@users.noreply.github.com>
2022-12-09 23:53:09 +0100

    Update WMZF_project.py

--- Project_for_WMZF/WMZF_project.py
+++ Project_for_WMZF/WMZF_project.py
@@ -1,9 +1,9 @@
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
 
 a = 0
 while a == 0:
-    opcja_1 = input('Wybierz rodzaj drgań;\nWpisz 1 dla drgań mechanicznych lub 2 dla drgań elektrycznych: ')
+    opcja_1 = input('Wybierz rodzaj drgań;\nWpisz 1 dla drgań mechanicznych lub 2 dla drgań elektrycznych:\n')
     try:
         opcja_1 = int(opcja_1)
         if (opcja_1 == 1 or opcja_1 == 2):
@@ -18,57 +18,32 @@ while a == 0:
             wybor = int(input('Wpisz 1, aby otrzymać wartość wychylenia w konkretnej chwili czasu lub 2, aby wyświetlić wykres drgań w funkcji czasu:'))
             match wybor:
                 case 1:
-                    t = float(input('Podaj wartosc czasu: '))
+                    t = float(input('Podaj moment ruchu:[s]'))
                     b = 1
                     def drgania_mech():
-                        a = float(input('Podaj wartosc amplitudy dla drgan mechanicznych: '))  # amplituda
-                        w = float(input('Podaj wartosc czestosci wlasnej dla drgan mechanicznych: '))  # czestosc wlasna
-                        f = float(input('Podaj wartosc wspolczynnika oporu ruchu dla drgan mechanicznych: '))  # wsp_op_ruchu
-                        m = float(input('Podaj wartosc masy dla drgan mechanicznych: '))  # masa
-                        p = float(input('Podaj wartosc fazy poczatkowej dla drgan mechanicznych: '))  # faza_poczatkowa
+                        a = float(input('Podaj wartość amplitudy drgań mechanicznych [m]:'))
+                        w = float(input('Podaj wartość czestości własnej drgań [1/s]:'))
+                        f = float(input('Podaj wartość współczynnika oporów ruchu [kg/s]:'))
+                        m = float(input('Podaj masę drgającego ciała [kg]:'))
+                        p = float(input('Podaj fazę poczatkową ruchu [rad]:'))
                         x = a * (np.exp(-f / (2 * m)) * t) * np.sin((np.sqrt((w ** 2) - ((f / (2 * m)) ** 2)) * t) + p)
                         return x
                     x = drgania_mech()
                     print('Wartość wychylania dla chwili czasu t=',t,'s wyniosła ',x)
                 case 2:
-                    a = float(input('Podaj wartosc amplitudy dla drgan mechanicznych: '))  # amplituda
-                    w = float(input('Podaj wartosc czestosci wlasnej dla drgan mechanicznych: '))  # czestosc wlasna
-                    f = float(input('Podaj wartosc wspolczynnika oporu ruchu dla drgan mechanicznych: '))  # wsp_op_ruchu
-                    m = float(input('Podaj wartosc masy dla drgan mechanicznych: '))  # masa
-                    p = float(input('Podaj wartosc fazy poczatkowej dla drgan mechanicznych: '))  # faza_poczatkowa
+                    a = float(input('Podaj wartość amplitudy drgań mechanicznych [m]:'))
+                    w = float(input('Podaj wartość czestości własnej drgań [1/s]:'))
+                    f = float(input('Podaj wartość współczynnika oporów ruchu [kg/s]:'))
+                    m = float(input('Podaj masę drgającego ciała [kg]:'))
+                    p = float(input('Podaj fazę poczatkową ruchu [rad]:'))
                     t = np.linspace(0, 1000, 100)
-                    x =a * (np.exp(-f / (2 * m)) * t) * np.sin((np.sqrt((w ** 2) - ((f / (2 * m)) ** 2)) * t) + p)
-                    plt.plot(x,t)
+                    x = a * (np.exp(-f / (2 * m)) * t) * np.sin((np.sqrt((w ** 2) - ((f / (2 * m)) ** 2)) * t) + p)
+                    plt.plot(x,t,color='r',label='Drgania mechaniczne: x(t)')
                     plt.show()
-
-
-
-
                     b = 1
                 case _:
-                    print("Nie mozna tego uzyc!Wybierz 1 albo 2")
+                    print("Nie można tego użyć. Wybierz 1 lub 2.")
         if opcja_1==2:
         #a tu RLC
             print(455677777)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
