4c7ca4369f0b8fae729785d4c7144b56063921db
Kamil Nowak <s189406@o365.student.pg.edu.pl>
2022-12-11 14:11:51 +0100

    Kejmil

--- Project_for_WMZF/WMZF_project.py
+++ Project_for_WMZF/WMZF_project.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+#WYBÓR: RODZAJ DRGAŃ
 a = 0
 while a == 0:
     opcja_1 = input('Wybierz rodzaj drgań;\nWpisz 1 dla drgań mechanicznych lub 2 dla drgań elektrycznych:\n')
@@ -12,38 +13,44 @@ while a == 0:
             print("Nie można tego użyć. Wybierz 1 lub 2.")
     except:
         print("To nie jest liczba. Wprowadź 1 lub 2.")
+
+#WYBÓR: MECHANICZNE - WARTOŚĆ LUB WYKRES
     if opcja_1 == 1:
+        a = float(input('Podaj wartość amplitudy drgań mechanicznych [m]:'))
+        w = float(input('Podaj wartość czestości własnej drgań [1/s]:'))
+        f = float(input('Podaj wartość współczynnika oporów ruchu [kg/s]:'))
+        m = float(input('Podaj masę drgającego ciała [kg]:'))
+        p = float(input('Podaj fazę poczatkową ruchu [rad]:'))
         b = 0
         while b == 0:
             wybor = int(input('Wpisz 1, aby otrzymać wartość wychylenia w konkretnej chwili czasu lub 2, aby wyświetlić wykres drgań w funkcji czasu:'))
             match wybor:
+#MECH-WARTOŚĆ
                 case 1:
                     t = float(input('Podaj moment ruchu:[s]'))
                     b = 1
-                    def drgania_mech():
-                        a = float(input('Podaj wartość amplitudy drgań mechanicznych [m]:'))
-                        w = float(input('Podaj wartość czestości własnej drgań [1/s]:'))
-                        f = float(input('Podaj wartość współczynnika oporów ruchu [kg/s]:'))
-                        m = float(input('Podaj masę drgającego ciała [kg]:'))
-                        p = float(input('Podaj fazę poczatkową ruchu [rad]:'))
+                    def mech_wartosc():
                         x = a * (np.exp(-f / (2 * m)) * t) * np.sin((np.sqrt((w ** 2) - ((f / (2 * m)) ** 2)) * t) + p)
                         return x
-                    x = drgania_mech()
+                    x = mech_wartosc()
                     print('Wartość wychylania dla chwili czasu t=',t,'s wyniosła ',x)
+#MECH-WYKRES
                 case 2:
-                    a = float(input('Podaj wartość amplitudy drgań mechanicznych [m]:'))
-                    w = float(input('Podaj wartość czestości własnej drgań [1/s]:'))
-                    f = float(input('Podaj wartość współczynnika oporów ruchu [kg/s]:'))
-                    m = float(input('Podaj masę drgającego ciała [kg]:'))
-                    p = float(input('Podaj fazę poczatkową ruchu [rad]:'))
-                    t = np.linspace(0, 1000, 100)
-                    x = a * (np.exp(-f / (2 * m)) * t) * np.sin((np.sqrt((w ** 2) - ((f / (2 * m)) ** 2)) * t) + p)
-                    plt.plot(x,t,color='r',label='Drgania mechaniczne: x(t)')
-                    plt.show()
+                    t = np.linspace(0, 100000, 1000)
                     b = 1
+                    def mech_wykres():
+                        x = a * (np.exp((-f)/(2 * m)) * t) * np.sin((np.sqrt((w ** 2) - ((f / (2 * m)) ** 2)) * t) + p)
+                        plt.plot(t,x,color='r')
+                        plt.title('Drgania mechaniczne: x(t)')
+                        plt.xlabel("Czas - t[s]")
+                        plt.ylabel("Wychylenie - x[m]")
+                        plt.grid()
+                        return plt.show()
+                    mech_wykres()
+
                 case _:
                     print("Nie można tego użyć. Wybierz 1 lub 2.")
-        if opcja_1==2:
-        #a tu RLC
-            print(455677777)
 
+#WYBÓR: ELEKTRYCZNE - WARTOŚĆ LUB WYKRES
+    if opcja_1==2:
+            print(455677777)
