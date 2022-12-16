cddfde83615b1d2770be0a1bd6d0ae7698acffd2
MarkPantuszew <119132215+MarkPantuszew@users.noreply.github.com>
2022-12-16 12:18:09 +0100

    Update WMZF_project.py

--- Project_for_WMZF/WMZF_project.py
+++ Project_for_WMZF/WMZF_project.py
@@ -39,24 +39,36 @@ while a == 0:
                     mech_wartosc()
 #MECH-WYKRES
                 case 2:
-                    t = np.linspace(0, 60, 100000)
-                    b = 1
+                    b=1
                     def mech_wykres():
-                        x = a * (np.exp((-f/(2 * m))*t)) * np.sin((np.sqrt(abs((w ** 2) - ((f / (2 * m)) ** 2))) * t) + p)
-                        plt.plot(t,x,color='r')
+                        t = np.linspace(0, 60, 1)
+                        t_data = []
+                        x_data = []
+                        x = a * (np.exp((-f / (2 * m)) * t)) * np.sin((np.sqrt(abs((w ** 2) - ((f / (2 * m)) ** 2))) * t) + p)
+                        fig, ax = plt.subplots()
+                        ax.set_xlim(0,60)
+                        ax.set_ylim(-(a + 1), (a + 1))
+                        line, = ax.plot(0, 0)
+                        plt.plot(t, x, color='y')
                         plt.title('Drgania mechaniczne: x(t)')
                         plt.xlabel("Czas - t[s]")
                         plt.ylabel("Wychylenie - x[m]")
                         plt.grid()
-                        if ((w**2) - ((f/(2 * m))**2)) >= 0:
+                        def animation_frame(t):
+                            t_data.append(t)
+                            x_data.append(a * (np.exp((-f / (2 * m)) * t)) * np.sin(
+                                (np.sqrt(abs((w ** 2) - ((f / (2 * m)) ** 2))) * t) + p))
+                            line.set_xdata(t_data)
+                            line.set_ydata(x_data)
+                            return line,
+                        animation = FuncAnimation(fig, func=animation_frame, frames=np.arange(0,60, 0.1), interval=1,blit=True, repeat=False)
+                        if ((w ** 2) - ((f / (2 * m)) ** 2)) >= 0:
                             return plt.show()
                         else:
                             return print('Brak drgań dla wprowadzonych parametrów, zachodzi zanik eksponencjalny')
                     mech_wykres()
-
                 case _:
                     print("Nie można tego użyć. Wybierz 1 lub 2.")
-
 #WYBÓR: ELEKTRYCZNE - WARTOŚĆ LUB WYKRES
     if opcja_1==2:
         a = float(input('Podaj wartość amplitudy ładunku [C]:'))
@@ -65,12 +77,10 @@ while a == 0:
         l = float(input('Podaj indukcyjność cewki w ukłdzie [H]:'))
         p = float(input('Podaj fazę poczatkową ruchu [rad]:'))
         b = 0
-
         while b == 0:
             wybor = int(input(
                 'Wpisz 1, aby otrzymać wartość ładunku w układzie w konkretnej chwili czasu lub 2, aby wyświetlić wykres drgań w funkcji czasu:'))
             match wybor:
-
                 # EL-WARTOŚĆ
                 case 1:
                     t = float(input('Podaj moment drgań:[s]'))
@@ -84,7 +94,6 @@ while a == 0:
                         else:
                             return print('Brak drgań dla wprowadzonych parametrów. Zachodzi zanik eksponencjalny')
                     el_wartosc()
-
                 # EL-WYKRES
                 case 2:
                     b = 1
@@ -96,26 +105,24 @@ while a == 0:
 
                         fig, ax = plt.subplots()
                         ax.set_xlim(0, 60)
-                        ax.set_ylim(-(x + 1), (x + 1))
+                        ax.set_ylim(-(a + 1), (a + 1))
                         line, = ax.plot(0, 0)
 
                         plt.title('Drgania elektryczne: q(t)')
                         plt.xlabel('Czas - t[s]')
                         plt.ylabel('Wartość łądunku w RLC - q[C]')
                         plt.grid()
-
                         def animation_frame(t):
                             t_data.append(t)
                             q_data.append(a * (np.exp((-r / (2 * l)) * t)) * np.sin((np.sqrt(abs((w ** 2) - ((r / (2 * l)) ** 2))) * t) + p))
                             line.set_xdata(t_data)
                             line.set_ydata(q_data)
                             return line,
-                        animation = FuncAnimation(fig,  func=animation_frame, frames=np.arange(0, 30, 0.1), interval=1,blit=True,repeat=False)
+                        animation = FuncAnimation(fig,  func=animation_frame, frames=np.arange(0, 60, 0.1), interval=1,blit=True,repeat=False)
                         if ((w ** 2) - ((r / (2 * l)) ** 2)) >= 0:
                             return plt.show()
                         else:
                             return print('Brak drgań dla wprowadzonych parametrów, zachodzi zanik eksponencjalny')
                     el_wykres()
-
                 case _:
                     print("Nie można tego użyć. Wybierz 1 lub 2.")
