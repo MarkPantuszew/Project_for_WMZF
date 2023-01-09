ef7e143ef3422d34c912b5eeea810d3fcd242776
Kamil Nowak <119288493+K4YM1L@users.noreply.github.com>
2023-01-09 12:21:11 +0100

    Update WMZF_project.py

--- Project_for_WMZF/WMZF_project.py
+++ Project_for_WMZF/WMZF_project.py
@@ -44,28 +44,32 @@ def wykres():
 clicks_mech,click_el = 0,0
 
 def mech():
+    global nowe_okno
+    nowe_okno = Toplevel(root)
+    nowe_okno.title("Drgania mechaniczne: x(t)")
+    nowe_okno.geometry("450x500")
     global title
     title = 'Drgania mechaniczne: x(t)'
     global ylabel
     ylabel = 'Wychylenie - x [m]'
     global jednostka
     jednostka = 'm'
-    l_wzor = Label(root, text='x = A * (exp(-f/(2m) * t)) * sin((sqrt(w^2 - ((f/2m)^2)) * t) + p)')
+    l_wzor = Label(nowe_okno, text='x = A * (exp(-f/(2m) * t)) * sin((sqrt(w^2 - ((f/2m)^2)) * t) + p)')
     global amplituda
-    amplituda = Entry(root, width=15)
-    l_amplituda = Label(root, text='Podaj wartosc \'A\' amplitudy drgan mechanicznych [m]:')
+    amplituda = Entry(nowe_okno, width=15)
+    l_amplituda = Label(nowe_okno, text='Podaj wartosc \'A\' amplitudy drgan mechanicznych [m]:')
     global omega
-    omega = Entry(root, width=15)
-    l_omega = Label(root, text='Podaj wartosc \'w\' czestosci wlasnej drgan [rad/s]:')
+    omega = Entry(nowe_okno, width=15)
+    l_omega = Label(nowe_okno, text='Podaj wartosc \'w\' czestosci wlasnej drgan [rad/s]:')
     global wsplop
-    wsplop = Entry(root, width=15)
-    l_wsplop = Label(root, text='Podaj wartosc \'f\' wspolczynnika oporow ruchu [kg/s]:')
+    wsplop = Entry(nowe_okno, width=15)
+    l_wsplop = Label(nowe_okno, text='Podaj wartosc \'f\' wspolczynnika oporow ruchu [kg/s]:')
     global masa
-    masa = Entry(root, width=15)
-    l_masa = Label(root, text='Podaj mase \'m\' drgajacego ciala [kg]:')
+    masa = Entry(nowe_okno, width=15)
+    l_masa = Label(nowe_okno, text='Podaj mase \'m\' drgajacego ciala [kg]:')
     global faza
-    faza = Entry(root, width=15)
-    l_faza = Label(root, text='Podaj faze \'p\' poczatkowa ruchu [rad]:')
+    faza = Entry(nowe_okno, width=15)
+    l_faza = Label(nowe_okno, text='Podaj faze \'p\' poczatkowa ruchu [rad]:')
     l_wzor.pack()
     l_amplituda.pack()
     amplituda.pack()
@@ -77,6 +81,14 @@ def mech():
     masa.pack()
     l_faza.pack()
     faza.pack()
+    lwybor = Label(nowe_okno, text='Wybierz wykres lub wartosc drgan dla konkretnej chwili czasu:')
+    bwykres = Button(nowe_okno, text='Wykres', command=tkwykres)
+    bwartosc = Button(nowe_okno, text='Wartosc', command=tkwartosc1)
+    exit = Button(nowe_okno, text="Wyjscie", command=root.destroy)
+    lwybor.pack()
+    bwykres.pack()
+    bwartosc.pack()
+    exit.pack()
     global clicks_mech
     clicks_mech += 1
     if clicks_mech == 1:
@@ -84,28 +96,32 @@ def mech():
         elbutton.config(state="disabled")
 
 def el():
+    global nowe_okno
+    nowe_okno = Toplevel(root)
+    nowe_okno.title("Drgania elektryczne: x(t)")
+    nowe_okno.geometry("500x450")
     global title
     title = 'Drgania elektryczne: q(t)'
     global ylabel
     ylabel = 'Wartosc ladunku - q [C]'
     global jednostka
     jednostka = 'C'
-    l_wzor = Label(root, text='x = Q * (exp(-R/(2L) * t)) * sin((sqrt(w^2 - ((R/2L)^2)) * t) + p)')
+    l_wzor = Label(nowe_okno, text='x = Q * (exp(-R/(2L) * t)) * sin((sqrt(w^2 - ((R/2L)^2)) * t) + p)')
     global amplituda
-    amplituda = Entry(root, width=15)
-    l_amplituda = Label(root, text='Podaj wartosc \'Q\' amplitudy ladunku w ukladzie [C]:')
+    amplituda = Entry(nowe_okno, width=15)
+    l_amplituda = Label(nowe_okno, text='Podaj wartosc \'Q\' amplitudy ladunku w ukladzie [C]:')
     global omega
-    omega = Entry(root, width=15)
-    l_omega = Label(root, text='Podaj wartosc \'w\' czestosci wlasnej drgan [rad/s]:')
+    omega = Entry(nowe_okno, width=15)
+    l_omega = Label(nowe_okno, text='Podaj wartosc \'w\' czestosci wlasnej drgan [rad/s]:')
     global wsplop
-    wsplop = Entry(root, width=15)
-    l_wsplop = Label(root, text='Podaj wartosc \'R\' rezystancji opornika [ohm]:')
+    wsplop = Entry(nowe_okno, width=15)
+    l_wsplop = Label(nowe_okno, text='Podaj wartosc \'R\' rezystancji opornika [ohm]:')
     global masa
-    masa = Entry(root, width=15)
-    l_masa = Label(root, text='Podaj indukcyjnosc cewki \'L\' w ukladzie [H]:')
+    masa = Entry(nowe_okno, width=15)
+    l_masa = Label(nowe_okno, text='Podaj indukcyjnosc cewki \'L\' w ukladzie [H]:')
     global faza
-    faza = Entry(root, width=15)
-    l_faza = Label(root, text='Podaj faze \'p\' poczatkowa ruchu [rad]:')
+    faza = Entry(nowe_okno, width=15)
+    l_faza = Label(nowe_okno, text='Podaj faze \'p\' poczatkowa ruchu [rad]:')
     l_wzor.pack()
     l_amplituda.pack()
     amplituda.pack()
@@ -117,6 +133,14 @@ def el():
     masa.pack()
     l_faza.pack()
     faza.pack()
+    lwybor = Label(nowe_okno, text='Wybierz wykres lub wartosc drgan dla konkretnej chwili czasu:')
+    bwykres = Button(nowe_okno, text='Wykres', command=tkwykres)
+    bwartosc = Button(nowe_okno, text='Wartosc', command=tkwartosc1)
+    exit = Button(nowe_okno, text="Wyjscie", command=root.destroy)
+    lwybor.pack()
+    bwykres.pack()
+    bwartosc.pack()
+    exit.pack()
     global click_el
     click_el+=1
     if click_el == 1:
@@ -155,59 +179,44 @@ def tkwartosc1():
         global p
         p = float(faza.get())
     except ValueError:
-        messagebox.showerror("Bląd!", "Proszę podać wartosći liczbowe")
+        messagebox.showerror("Błąd!", "Proszę podać wartości liczbowe")
     if float(masa.get()) == 0:
-        messagebox.showerror("Bład", "Nie mogę wykonać dzielenia przez 0!")
+        messagebox.showerror("Błąd!", "Nie mogę wykonać dzielenia przez 0!")
     else:
         global chwilat
-        chwilat = Entry(root, width=15)
-        l_chwilat = Label(root, text='Wprowadz chwile czasu \'t\' trwania drgania [s]:')
+        chwilat = Entry(nowe_okno, width=15)
+        l_chwilat = Label(nowe_okno, text='Wprowadz chwile czasu \'t\' trwania drgania [s]:')
         l_chwilat.pack()
         chwilat.pack()
-        zatwierdz = Button(root, text='Zatwierdz', command=tkwartosc2)
+        zatwierdz = Button(nowe_okno, text='Zatwierdz', command=tkwartosc2)
         zatwierdz.pack()
 
 def tkwartosc2():
+    global t
     try:
-        global t
         t = float(chwilat.get())
     except ValueError:
         messagebox.showerror("Bląd!", "Proszę podać wartosći liczbowe")
     if float(masa.get()) == 0:
         messagebox.showerror("Bład", "Nie mogę wykonać dzielenia przez 0!")
     else:
-        str_t = str(t)
-        wartosc()
+        str_t = str((chwilat.get()))
+        x = str(wartosc())
         # zapis()
-        l_wartosc = Label(root, text='Wartość drgania dla t =' + str_t + 's wyniosła:\n' + jednostka)
+        l_wartosc = Label(root, text='Wartość drgania dla t =' + str_t + 's wyniosła:\n' + x + jednostka)
         l_wartosc.pack()
 
 def zamykanie_okna():
     if messagebox.askokcancel("Zamykanie okna","Chcesz zamknąć okno?"):
         root.destroy()
 root.protocol("WM_DELETE_WINDOW",zamykanie_okna)
-
 #wybór rodzaj drgań
 root.title("Drgania")
-root.geometry("400x500")
+root.geometry("300x80")
 lrodzaj = Label(root, text='Wybierz rodzaj tłumionych drgan harmonicznych:')
-
 mechbutton = Button(root, text='Mechaniczne', command=mech, fg='red',)
 elbutton = Button(root, text='Elektryczne', command=el, fg='blue')
-
 lrodzaj.pack()
 mechbutton.pack()
 elbutton.pack()
-
-#wykres/wartość
-lwybor = Label(root, text='Wybierz wykres lub wartosc drgan dla konkretnej chwili czasu:')
-bwykres = Button(root, text='Wykres', command = tkwykres)
-bwartosc = Button(root, text='Wartosc', command=tkwartosc1)
-lwybor.pack()
-bwykres.pack()
-bwartosc.pack()
-
-#przycisk wyjścia
-exit = Button(root, text="Wyjscie", command=root.destroy)
-exit.pack()
 root.mainloop()
